3620a9c7416fcd518800fa2b6a29bf7cae3bebb7
med-amine01 <MohamedAmine.CHEBBI@tek-up.de>
2023-11-13 10:04:02 +0100

    api gateway filter

---
+++ api-gateway/src/main/java/de/tekup/apigateway/filter/RouteValidator.java
@@ -0,0 +1,23 @@
+package de.tekup.apigateway.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/v1/auth/token",
+            "/api/v1/users",
+            "/eureka"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
